9ac12f7f82d6f8ed3a2a01c997cdaa3ec2c148b0
Jacopo Grazzini <gjacopo@gmail.com>
2020-09-09 19:19:35 +0200

    Update Ridge.R

--- model/Ridge.R
+++ model/Ridge.R
@@ -9,9 +9,11 @@ set.seed(1)
 
 n <- 200  # Number of observations
 p <- 300  # Number of predictors included in model
-beta<- c(1/(1:p)^2) #approximately sparse model, slope coefficients small but not zero
+beta<- c(1/(1:p)^2)
+#approximately sparse model, slope coefficients small but not zero
 x <- matrix(rnorm(n*p), nrow=n, ncol=p)
-y <- x%*%beta + rnorm(n) #generate the dependant variable y
+y <- x%*%beta + rnorm(n)
+#generate the dependant variable y
 
 fit.ridge <- glmnet(x, y, family="gaussian", alpha=0)
 
